21f353b47881fd46373c78779b70eabbbc5c9341
soulmateafk <etdjohancerinza@gmail.com>
2025-10-23 01:24:03 -0500

    angular

---
+++ src/app/app.routes.ts
@@ -0,0 +1,15 @@
+import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import path from 'path';
+import { About } from './pages/about/about';
+import { Products } from './pages/products/products';
+import { Component } from '@angular/core';
+import { Contact } from './pages/contact/contact';
+
+export const routes: Routes = [
+    {path:'', component:Home},
+    {path:'nosotros', component:About},
+    {path:'productos', component:Products},
+    {path:'contacto', component:Contact},
+    {path:'**', redirectTo:''}
+];
