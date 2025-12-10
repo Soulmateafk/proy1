c745cdcfe9c5ac0ccebeba3ed703dba56e103d77
Soulmateafk <etdjohancerinza@gmail.com>
2025-12-09 19:49:53 -0500

    contact3

--- src/app/services/products.ts
+++ src/app/services/products.ts
@@ -21,42 +21,42 @@ export class Products {
       descripcion : 'Description of Product1',
       precio: 5500000000
 ,
-      imagen: '/touh.jpg',
+      imagen: 'assets/touh.jpg',
     },
     {
       id: 2,
       nombre: 'Suzuki GSX-R1000R',
       descripcion : 'moto deportiva de alto rendimiento ',
       precio: 98900000,
-      imagen: '/gsx.jpg',
+      imagen: 'assets/gsx.jpg',
        },
     {
       id: 3,
       nombre: 'CFMOTO 250SR',
       descripcion : ' deportiva 250cc diseño en blanco ',
       precio: 17990000 ,
-      imagen: '/galgo.webp',
+      imagen: 'assets/galgo.webp',
          },
     {
       id: 4,
       nombre: 'Suzuki Gixxer 250',
       descripcion : 'susuki de motor 249cc',
       precio: 15580000,
-      imagen: '/250cc.webp',
+      imagen: 'assets/250cc.webp',
                },
     {
       id: 5,
       nombre: 'Yamaha Tracer 9 GT',
       descripcion : 'nueva marca de moto yamaha',
       precio: 77000000.0,
-      imagen: '/tracer.webp',
+      imagen: 'assets/tracer.webp',
                },
     {
       id: 6,
       nombre: 'yamaha XSR900',
       descripcion : 'moto de alto cilindraje yamaha',
       precio: 67000000.0,
-      imagen: '/yamaha.jpg',
+      imagen: 'assets/yamaha.jpg',
    }
                 
   ]
